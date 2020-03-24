2e8acf8ba4a389852d9321817edcd64ec388b63f
Marta <martaswierczewska@wp.pl>
2020-03-24 10:00:10 +0100

    plan for updating

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,12 +10,18 @@ public class Main {
         board.create();
         board.fillWithValues();
 
-
         List<Pair<Integer, Integer>> emergencyExits = Arrays.asList( new Pair<>(2,0), new Pair<>(7,0), new Pair<>(4,4));
         board.addEmergencyExit(emergencyExits);
 
+        int i=0;
+        while(i<5){
+//            tu funkcja obliczajaca najblizsze exit i przemiszczanie pozaru i zmieniajaca polozenie
+//            jakies board.update()
+//             i wtedy znowu board show
+//             a w while funkcja isPersonOnBoard() vczy cos takiego
+            board.show();
+            i++;
+        }
 
-
-        board.show();
     }
 }
